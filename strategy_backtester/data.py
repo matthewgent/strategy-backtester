bcdc80880165b56e244a60861f08246a6753886d
matthewgent <matthewgent1@gmail.com>
2025-08-27 21:27:44 +0100

    Began adding data functions.

--- strategy_backtester/data.py
+++ strategy_backtester/data.py
@@ -1,9 +1,24 @@
+from datetime import date
 from polygon import RESTClient
+from polygon.rest.models import Agg
 
-def retrieve(ticker: str) ->
+
+def retrieve(ticker: str, start: date, end: date) -> list[Agg]:
     client = RESTClient()
     aggs = client.get_aggs(
         ticker,
         1,
-        "day"
+        "day",
+        start.isoformat(),
+        end.isoformat(),
     )
+
+    return aggs
+
+
+def store(aggs: list[Agg]) -> Agg:
+    # store all the aggs
+
+
+results = retrieve("AAPL", date(2025, 8, 1), date(2025, 8, 7))
+print(results)
